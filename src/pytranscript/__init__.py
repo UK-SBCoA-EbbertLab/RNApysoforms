40e21ea24c187199ebff43773c90111ef2c517ec
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-09-26 11:17:10 -0400

    update

--- src/pytranscript/__init__.py
+++ src/pytranscript/__init__.py
@@ -3,9 +3,7 @@ from .geom_intron import geom_intron
 from .shorten_gaps import shorten_gaps
 from .to_intron import to_intron
 from .set_axis import set_axis
-from .calculate_cds_exon_difference import calculate_cds_exon_difference
-from .rescale_cds import rescale_cds
 from .read_gtf import read_gtf
 
 __all__ = ['geom_range', 'geom_intron', 'shorten_gaps', 
-           'to_intron', 'set_axis', 'calculate_cds_exon_difference', 'rescale_cds', "read_gtf"]
+           'to_intron', 'set_axis', "read_gtf"]
--- test_scripts/test_load_gtf.py
+++ test_scripts/test_load_gtf.py
@@ -1,6 +1,7 @@
 ## Import libraries
 from pytranscript import read_gtf
 import polars as pl
+import pandas as pd
 
 ## Read gtf
 annotations = read_gtf("./test_data/Homo_sapiens.GRCh38.112.chr21-22.gtf")
@@ -9,3 +10,4 @@ annotations = read_gtf("./test_data/Homo_sapiens.GRCh38.112.chr21-22.gtf")
 ## Filter gene of interest
 annotation = annotations.filter(pl.col("gene_name") == "APP")
 
+annotation_pandas = annotation.to_pandas()
