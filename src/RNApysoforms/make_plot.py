b58f9c39fca28aa418da2f64d51094d969071277
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-10-07 14:40:08 -0500

    update

--- src/RNApysoforms/make_plot.py
+++ src/RNApysoforms/make_plot.py
@@ -50,60 +50,52 @@ def make_plot(
     Returns:
     - go.Figure: The finalized Plotly figure with three subplots.
     """
+    if expression_traces != None:
+        full_trace_list = transcript_structure_traces + expression_traces
+    else:
+        full_trace_list = [transcript_structure_traces]
 
     # Initialize the subplot figure with 1 row and dynamic number of columns
     fig = make_subplots(
         rows=1,
-        cols=(len(expression_traces) + 1) if expression_traces is not None else 1,
+        cols=len(full_trace_list),
         subplot_titles=subplot_titles,
         horizontal_spacing=horizontal_spacing,
         vertical_spacing=vertical_spacing
     )
 
-    # Batch Add Transcript Structure Traces to the First Subplot
-    fig.add_traces(transcript_structure_traces,
-        rows=[1] * len(transcript_structure_traces),
-        cols=[1] * len(transcript_structure_traces)
+    for i in range(len(full_trace_list)):
+
+        # Batch Add Transcript Structure Traces to the First Subplot
+        fig.add_traces(full_trace_list[i],
+            rows=[1] * len(full_trace_list[i]),
+            cols=[i+1] * len(full_trace_list[i]))
+
+
+    fig.update_layout(
+        row=1,
+        col=1,
         yaxis=dict(
-        tickvals=list(range(annotation.n_unique(subset="transcript_id"))),               # Positions of the ticks
-        ticktext=annotation.select("transcript_id").unique(maintain_order=True).to_series().to_list(),  # Custom labels for the ticks
-        tickfont=dict(size=10, family='DejaVu Sans', color='black')))
-
-    # Batch Add Counts Traces to the Second Subplot
-    if expression_traces != None:
-
-        for i in range(len(expression_traces)):
-            fig.add_traces(
-                expression_traces[i],
-                rows=[1] * len(expression_traces[i]),
-                cols=[i+2] * len(expression_traces[i]),
-                yaxis=dict(showticklabels=False)
-            )
-
-
-
-    # # Customize Axes Titles if Provided
-    # if axis_titles:
-    #     # Transcript Structure Subplot
-    #     ts_titles = axis_titles.get("Transcript Structure", {})
-    #     if "x" in ts_titles:
-    #         fig.update_xaxes(title_text=ts_titles["x"], row=1, col=1)
-    #     if "y" in ts_titles:
-    #         fig.update_yaxes(title_text=ts_titles["y"], row=1, col=1)
-
-    #     # Counts Subplot
-    #     counts_titles = axis_titles.get("Counts", {})
-    #     if "x" in counts_titles:
-    #         fig.update_xaxes(title_text=counts_titles["x"], row=1, col=2)
-    #     if "y" in counts_titles:
-    #         fig.update_yaxes(title_text=counts_titles["y"], row=1, col=2)
-
-    #     # Relative Abundance Subplot
-    #     ra_titles = axis_titles.get("Relative Abundance", {})
-    #     if "x" in ra_titles:
-    #         fig.update_xaxes(title_text=ra_titles["x"], row=1, col=3)
-    #     if "y" in ra_titles:
-    #         fig.update_yaxes(title_text=ra_titles["y"], row=1, col=3)
+            tickvals=list(range(annotation.n_unique(subset="transcript_id"))),               # Positions of the ticks
+            ticktext=annotation.select("transcript_id").unique(maintain_order=True).to_series().to_list(),  # Custom labels for the ticks
+            tickfont=dict(size=10, family='DejaVu Sans', color='black')),
+        xaxis=dict(showticklabels=False),
+        legend=dict(traceorder="reversed"),
+        hoverlabel=dict(font=dict(size=8)),
+        showlegend=True,
+        yaxis_title="",
+        xaxis_title="") 
+    
+    for i in range(2, (len(expression_traces) + 2)):
+        fig.update_layout(
+            row=1,
+            col=i,
+            yaxis=dict(showticklabels=False),
+            legend=dict(traceorder="reversed"),
+            hoverlabel=dict(font=dict(size=8)),
+            showlegend=True,
+            yaxis_title="",
+            xaxis_title="") 
 
     # Update Overall Layout
     fig.update_layout(
