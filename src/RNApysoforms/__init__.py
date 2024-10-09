ebdcd8bb1394bfad3460779d4a0ebd15e12f55f2
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-10-09 18:36:28 -0500

    up

--- src/RNApysoforms/__init__.py
+++ src/RNApysoforms/__init__.py
@@ -3,11 +3,9 @@ from .shorten_gaps import shorten_gaps  # Function to shorten gaps in data
 from .to_intron import to_intron        # Function to convert exons to introns
 from .set_axis import set_axis          # Function to set axis properties for plot
 from .read_gtf import read_gtf          # Function to read and parse GTF (Gene Transfer Format) files
-from .make_transcript_structure_traces import make_transcript_structure_traces    # Function to generate traces for plotting
 from .read_expression_matrix import read_expression_matrix # Function to load counts matrix
 from .gene_filtering import gene_filtering # Function to filter by gene_name 
 from .calculate_exon_number import calculate_exon_number ## Function to calculate exon number if missing
-from .make_transcript_expression_traces import make_transcript_expression_traces ## Function to make transcript expression traces for boxplots
 from .make_plot import make_plot
 from .make_traces import make_traces
 
