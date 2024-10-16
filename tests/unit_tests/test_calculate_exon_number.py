6b5956c2ac8655390d75392900b859b500ae0543
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-10-16 08:34:32 -0500

    testing updates

--- tests/unit_tests/test_calculate_exon_number.py
+++ tests/unit_tests/test_calculate_exon_number.py
@@ -90,93 +90,41 @@ def test_non_polars_dataframe():
 
 def test_complex_transcript_structure():
 
-    # Get the directory of the current file
-    current_dir = os.path.dirname(__file__)
-    # Construct the path to the test_data directory
-    test_data_dir = os.path.abspath(os.path.join(current_dir, '../../', 'test_data'))
-    # Define file paths
-    gtf_path = os.path.join(test_data_dir, "Homo_sapiens_chr21_and_Y.GRCh38.110.gtf")
-    # Read gtf
-    annotation = pt.read_ensembl_gtf("./test_data/Homo_sapiens_chr21_and_Y.GRCh38.110.gtf")
-    # Add introns
-    annotation = pt.to_intron(annotation)
-    # Drop exon number
-    annotation_2 = annotation.drop("exon_number")
-    # Recalculate exon number
-    annotation_2 = pt.calculate_exon_number(annotation_2)
 
-
-    # Annotations with multiple transcripts, exons, introns, and CDS regions
     df = pl.DataFrame({
         "transcript_id": [
-            # tx1 (positive strand)
-            "tx1", "tx1", "tx1", "tx1", "tx1", "tx1", "tx1",
-            # tx2 (negative strand)
-            "tx2", "tx2", "tx2", "tx2", "tx2", "tx2", "tx2",
-            # tx3 (positive strand)
-            "tx3", "tx3", "tx3", "tx3", "tx3", "tx3", "tx3"
+            "tx1", "tx1", "tx1", "tx1", "tx1", "tx1", "tx1", "tx1",  # tx1 (positive strand)
+            "tx2", "tx2", "tx2", "tx2", "tx2", "tx2", "tx2", "tx2",  # tx2 (negative strand)
+            "tx3", "tx3", "tx3", "tx3", "tx3", "tx3", "tx3", "tx3"   # tx3 (positive strand)
         ],
         "start": [
-            # tx1
-            100, 150, 201, 301, 301, 501, 501,
-            # tx2 (negative strand)
-            400, 450, 601, 701, 701, 801, 801,
-            # tx3
-            900, 950, 1001, 1101, 1101, 1301, 1301
+            100, 150, 201, 301, 301, 401, 501, 501,  # tx1
+            400, 450, 501, 601, 601, 701, 801, 801,  # tx2
+            900, 950, 1001, 1101, 1101, 1201, 1301, 1301  # tx3
         ],
         "end": [
-            # tx1
-            200, 200, 300, 400, 400, 600, 550,
-            # tx2
-            500, 500, 700, 800, 800, 900, 850,
-            # tx3
-            1000, 1000, 1100, 1200, 1200, 1400, 1350
+            200, 200, 300, 400, 400, 500, 600, 550,  # tx1
+            500, 500, 600, 700, 700, 800, 900, 850,  # tx2
+            1000, 1000, 1100, 1200, 1200, 1300, 1400, 1350  # tx3
         ],
         "type": [
-            # tx1
-            "exon",    # Exon 1: 100-200
-            "CDS",     # CDS 1: 150-200 (within Exon 1)
-            "intron",  # Intron 1: 201-300
-            "exon",    # Exon 2: 301-400
-            "CDS",     # CDS 2: 301-400 (within Exon 2)
-            "exon",    # Exon 3: 501-600
-            "CDS",     # CDS 3: 501-550 (within Exon 3)
-            # tx2
-            "exon",    # Exon 1: 400-500
-            "CDS",     # CDS 3: 450-500 (within Exon 1)
-            "intron",  # Intron 1: 501-600
-            "exon",    # Exon 2: 601-700
-            "CDS",     # CDS 2: 601-700 (within Exon 2)
-            "exon",    # Exon 3: 701-900
-            "CDS",     # CDS 1: 801-850 (within Exon 3)
-            # tx3
-            "exon",    # Exon 1: 900-1000
-            "CDS",     # CDS 1: 950-1000 (within Exon 1)
-            "intron",  # Intron 1: 1001-1100
-            "exon",    # Exon 2: 1101-1200
-            "CDS",     # CDS 2: 1101-1200 (within Exon 2)
-            "exon",    # Exon 3: 1301-1400
-            "CDS"      # CDS 3: 1301-1350 (within Exon 3)
+            "exon", "CDS", "intron", "exon", "CDS", "intron", "exon", "CDS",  # tx1
+            "exon", "CDS", "intron", "exon", "CDS", "intron", "exon", "CDS",  # tx2
+            "exon", "CDS", "intron", "exon", "CDS", "intron", "exon", "CDS"   # tx3
         ],
         "strand": [
-            # tx1
-            "+", "+", "+", "+", "+", "+", "+",
-            # tx2
-            "-", "-", "-", "-", "-", "-", "-",
-            # tx3
-            "+", "+", "+", "+", "+", "+", "+"
+            "+", "+", "+", "+", "+", "+", "+", "+",  # tx1
+            "-", "-", "-", "-", "-", "-", "-", "-",  # tx2
+            "+", "+", "+", "+", "+", "+", "+", "+"   # tx3
         ]
     })
 
-    # Expected exon numbers for each row
     expected_exon_numbers = [
-        # tx1
-        1, 1, 1, 2, 2, 3, 3,
-        # tx2 (negative strand)
-        3, 3, 3, 2, 2, 1, 1,
-        # tx3
-        1, 1, 1, 2, 2, 3, 3
+        1, 1, 1, 2, 2, 2, 3, 3,  # tx1
+        3, 3, 2, 2, 2, 1, 1, 1,  # tx2
+        1, 1, 1, 2, 2, 2, 3, 3   # tx3
     ]
+
 
     # Call the function to calculate exon numbers
     result = calculate_exon_number(df)
