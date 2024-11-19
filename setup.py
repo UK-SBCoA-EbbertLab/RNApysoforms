e07b658c19853d7445c20b68afa9d1d655c896ca
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-11-19 11:23:13 -0500

    Updated version to 0.9.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RNApysoforms",
-    version="0.9.4",
+    version="0.9.5",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
